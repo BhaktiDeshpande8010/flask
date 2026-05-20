58b25e97405ea9700cd84c184d8323191408bba6
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-05-20 23:15:45 +0530

    storing data directly in sqlite

--- flask_mini/app.py
+++ flask_mini/app.py
@@ -15,5 +15,31 @@ def register():
 def service():
     return render_template("about.html")
 
+@app.route("/saveform", methods=["POST", "GET"])
+def saveform():
+    if request.method == "POST":
+        nm = request.form["name"]
+        em = request.form["email"]
+        ps = request.form["password"]
+        cn = request.form["contact"]
+
+        import sqlite3 as sq
+
+        con = sq.connect("Flask.db")
+        cur = con.cursor()
+        cur.execute(
+            "insert into registration(name,email,password,contact) values(?,?,?,?)",
+            (nm, em, ps, cn)
+        )
+        
+        con.commit()
+        con.close()
+
+        return "Registration Successful"
+
+    else:
+        return "failed"
+
 if __name__=="__main__":
     app.run(debug=True)
+
