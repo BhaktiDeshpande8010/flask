3b70da3cbdf0faebb9def997c3a9776b1977f81b
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-05-26 23:33:55 +0530

    Logic

--- web_templating.py
+++ web_templating.py
@@ -2,9 +2,16 @@ from flask import *
 
 app = Flask(__name__)   # helps to run a particular file
 
+# BASIC FILE RENDERING 
+@app.route("/")
+def home():
+    # student_list=[12,34,'hello',67, True]
+    student_list={1:200,2:300,3:400,4:500,6:700}
+    return render_template("home.html",s_list=student_list)
+
+
 @app.route("/about")
 def about():
-
     name = "python"
     return render_template("about.html", studentname=name)
 
