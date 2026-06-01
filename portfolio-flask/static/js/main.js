5ddc3cf5a43a32667e4270c0950045c07016b24b
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-06-01 23:58:08 +0530

    changes

---
+++ portfolio-flask/static/js/main.js
@@ -0,0 +1,19 @@
+// SCROLL ANIMATION
+
+const animatedElements = document.querySelectorAll('.fade-up, .slide-in');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+
+    });
+}, {
+    threshold: 0.2
+});
+
+animatedElements.forEach((el) => {
+    observer.observe(el);
+});
